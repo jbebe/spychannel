92d54998ef683367d04de8097cdb77939358f001
Juhász Bálint <juhasz.balint.bebe@gmail.com>
2018-09-22 23:00:44 +0200

    create signal service (decomposition)

---
+++ Server/Signal/SpyChannelSignalService/CacheConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpyChannelSignalService
+{
+  class CacheConstants
+  {
+    public const string UserHashKey = "users";
+  }
+}
