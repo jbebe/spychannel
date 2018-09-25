4788a40b8b76116012b5a4259e773dbcf36ea512
Juhász Bálint <juhasz.balint.bebe@gmail.com>
2018-09-25 22:40:39 +0200

    create poc signalr on server side

--- Server/SpyChannelSignalServer/Startup.cs
+++ Server/SpyChannelSignalServer/Startup.cs
@@ -1,69 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.WebSockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using SpyChannel.SignalServer.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace SpyChannel.SignalServer
 {
   public class Startup
   {
     // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors();
+      services.AddSignalR();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
-      loggerFactory.AddConsole(LogLevel.Debug);
-      loggerFactory.AddDebug(LogLevel.Debug);
-
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
 
-      app.UseMvc(routes =>
+      app.UseCors(builder =>
+       builder
+       .AllowAnyOrigin()
+       .AllowAnyHeader()
+       .AllowAnyMethod()
+       .AllowCredentials()
+       .Build());
+
+      app.UseFileServer();
+
+      app.UseSignalR(routes =>
       {
-        routes.MapRoute(
-            name: "default",
-            template: "{controller=Home}/{action=Index}/{id?}");
+        routes.MapHub<ChatHub>("/chat");
       });
-
-      app.UseWebSockets();
-      app.UseSignalR();
     }
-
-    #region Echo
-
-    private async Task Echo(HttpContext context, WebSocket webSocket)
-    {
-      var buffer = new byte[1024 * 4];
-      
-      // wait for incoming ws connection to build up
-      WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-      // wait while the connection is not closed
-      while (!result.CloseStatus.HasValue)
-      {
-        // incoming message? respond with same data!
-        await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-
-        // wait for next message
-        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-      }
-      await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-    }
-    
-    #endregion
   }
 }
